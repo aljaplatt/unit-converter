d2bca4bc39bb43d11be77a1246da537ec173f38d
Alex Platt <alja.platt@gmail.com>
2022-02-15 17:44:56 +0000

    working

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const btn = document.querySelector(".btn");
 const input = document.querySelector(".input");
 
 //==========================
-let newNumber = 20;
 let number = 0;
 let numBox = document.querySelector(".num-box");
 //==========================
@@ -21,14 +20,18 @@ let calculatedGallons = 0;
 let calculatedLiters = 0;
 let initialCon = `${number} liters = ${calculatedGallons} gallons | ${number} gallons = ${calculatedLiters} liters`;
 //=============================
+const kg = 0.454;
+const pound = 2.204;
+let calculatedKgs = 0;
+let calculatedPounds = 0;
+let initCon = `${number} kilos = ${calculatedPounds} pounds | ${number} pounds = ${calculatedKgs} kilos`;
+//=============================
 
 meterFeet.textContent = initialString;
 litersGallons.textContent = initialCon;
-kgPounds.textContent = initialCon; // ***************
+kgPounds.textContent = initCon; // ***************
 
 function length() {
-  number = newNumber;
-  console.log(number);
   let res = number * meter;
   let conv = number * foot;
   let result = Math.round((res + Number.EPSILON) * 100) / 100;
@@ -41,28 +44,37 @@ function length() {
   initialString = `${number} meters = ${calculatedFeet} feet | ${number} feet = ${calculatedMeters} meters`;
   meterFeet.textContent = initialString;
 }
-
-btn.addEventListener("click", (e) => {
-  e.preventDefault();
-  console.log(input.value);
-  newNumber = input.value;
-  numBox.textContent = input.value;
-  console.log(`numbox: ${numBox}`);
-  length();
-  volume();
-});
-
 function volume() {
-  number = newNumber;
   let res = number * gallon;
   let conv = number * liter;
   let result = Math.round((res + Number.EPSILON) * 100) / 100;
   let res2 = Math.round((conv + Number.EPSILON) * 100) / 100;
-  console.log(result.toFixed(3));
-  console.log(res2.toFixed(3));
   calculatedGallons = result;
   calculatedLiters = res2;
 
   initialCon = `${number} liters = ${calculatedGallons} gallons | ${number} gallons = ${calculatedLiters} liters`;
   litersGallons.textContent = initialCon;
 }
+
+function weight() {
+  let res = number * pound;
+  let conv = number * kg;
+  let result = Math.round((res + Number.EPSILON) * 100) / 100;
+  let res2 = Math.round((conv + Number.EPSILON) * 100) / 100;
+  calculatedPounds = result;
+  calculatedKgs = res2;
+
+  initCon = `${number} kilos = ${calculatedPounds} pounds | ${number} pounds = ${calculatedKgs} kilos`;
+  kgPounds.textContent = initCon;
+}
+
+btn.addEventListener("click", (e) => {
+  e.preventDefault();
+  console.log(input.value);
+  number = input.value;
+  numBox.textContent = input.value;
+  console.log(`numbox: ${numBox}`);
+  length();
+  volume();
+  weight();
+});
